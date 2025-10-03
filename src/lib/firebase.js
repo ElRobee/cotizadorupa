98ac1bc02a3be779abfeb05e5f753ff17f1d2d4a
ElRobee <robertoverdejo@gmail.com>
2025-10-03 10:36:57 -0300

    Create firebase.js

---
+++ src/lib/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBwRKeZr6dCV2FDo_n3Gide4EHS5r0YFt4",
+  authDomain: "cotizaciones-app-9bfd7.firebaseapp.com",
+  projectId: "cotizaciones-app-9bfd7",
+  storageBucket: "cotizaciones-app-9bfd7.firebasestorage.app",
+  messagingSenderId: "53634767010",
+  appId: "1:53634767010:web:71d2c6c51a042995b81ffb"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
